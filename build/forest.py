6d9c79ed48779a87c396ae7f40f1bb6716c88d6d
dmackinnon1 <dmackinnon1@gmail.com>
2018-06-11 20:58:11 -0400

    added explanation generation

--- build/forest.py
+++ build/forest.py
@@ -1,8 +1,8 @@
-
 #
-# Script to generate puzzles based on Raymond Smullyan's 'Alice in the Forest of Forgetfulness' 
+# Script to generate puzzles based on Raymond Smullyan's 
+# 'Alice in the Forest of Forgetfulness' puzzles from 
+# 'What is the Name of this Book?'
 #
-#########################
 
 # first we define the days of the week
 daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -79,14 +79,66 @@ def validPuzzle(lionStatement, unicornStatement, counter):
         puzzle['unicorn'] = ("The Unicorn says: " + unicornStatement['description'] + '.')
     puzzle['solution'] = validDays[0]
     puzzle['id'] = counter
+    puzzle['explanation'] = explanation1("Lion",lionStatement['days'], lionLying, lionPositive, lionNegative, lionDays)
+    puzzle['explanation'] = puzzle['explanation'] + " "
+    puzzle['explanation'] = puzzle['explanation'] + explanation1("Unicorn",unicornStatement['days'], unicornLying, unicornPositive, unicornNegative, unicornDays)
+    puzzle['explanation'] = puzzle['explanation'] + " Based on what both are saying, the only day it could be is " + validDays[0] +"."
     return puzzle
 
+def prettyList(list, conj):
+    isFirst = True;
+    result = ""
+    count = 1
+    size = len(list)
+    for s in list:
+        if not isFirst and size > 2:
+            result += ", "
+        isFirst  = False
+        if count == size and size > 1:
+            if (size == 2):
+                result += " "
+            result += conj + " "
+        count = count +1
+        result += s
+    return result
+
+def explanation1(actor, statement, lieDays, daysIfTrue, daysIfFalse, unionDays):    
+    exp1 = actor +" says today"
+    if len(statement) == 1:
+        exp1 += " is " + statement[0]
+    else:
+        exp1 += " could be " + prettyList(statement, "or")      
+    exp1 += ". We know that " + actor + " lies on " + prettyList(lieDays, "and")
+    exp1 += ". So if they are telling the truth it can"
+    if (len(daysIfTrue) == 0):
+        exp1 += " not be any day (so they must be lying)"
+    elif (len(daysIfTrue) == 1):
+        exp1 += " only be " + daysIfTrue[0]    
+    else:
+        exp1 += " be " + prettyList(daysIfTrue, "or")
+    exp1 += ", and if they are lying, it can"
+    if (len(daysIfFalse) == 0):
+        exp1 += " not be any day (so they must be telling the truth)"
+    elif (len(daysIfFalse) == 1):
+        exp1 += " only be " + daysIfFalse[0]    
+    else:
+        exp1 += " be " + prettyList(daysIfFalse, "or")
+    exp1 += ". All told, from what " + actor + " says, it could"
+    if (len(unionDays) == 1):
+        exp1 += " only be " + unionDays[0]    
+    else:
+        exp1 += " be " + prettyList(unionDays, "or")
+    exp1 += "."
+    return exp1
+     
+
 def jsonForPuzzle(puzzle):
-	json = '{"lion": "' + puzzle['lion'] +'",' + "\n"
-	json += '"unicorn": "' + puzzle['unicorn'] +'",' + "\n"
-	json += '"solution": "' + puzzle['solution'] +'",' + "\n"
-	json += '"id": "' + str(puzzle['id']) +'"}'
-	return json
+    json = '{"lion": "' + puzzle['lion'] +'",' + "\n"
+    json += '"unicorn": "' + puzzle['unicorn'] +'",' + "\n"
+    json += '"solution": "' + puzzle['solution'] +'",' + "\n"
+    json += '"explanation": "' + puzzle['explanation'] +'",' + "\n"
+    json += '"id": "' + str(puzzle['id']) +'"}'
+    return json
 
 counter = 0
 validPuzzles = []
